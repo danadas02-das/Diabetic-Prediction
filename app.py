498d48155022fd5bdd1c09c1fd2c8759bc2fbe7e
danadas02-das <danadas02@gmail.com>
2026-02-15 12:17:22 +0400

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import joblib
-model=joblib.load("Revenue.pkl")
-st.title("Cold stone Sales ")
-st.header("Revenue Prediction")
-user_int=st.number_input("Enter the Temperature")
-if st.button("Predict Revenue"):
-    st.write(model.predict([[user_int]]))
